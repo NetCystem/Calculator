b72d157d74dbba128087cfada8f3cae2e59624e1
NetCystem <netcystem@icloud.com>
2017-12-16 15:59:52 +0400

    comit

--- app/src/main/java/com/example/user/calculator/MainActivity.java
+++ app/src/main/java/com/example/user/calculator/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.user.calculator;
+
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,220 +31,205 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button num0;
     private Button spot;
     private Button equal;
+    private Button reset;
     private EditText txt;
-    private String counter;
-    List<Character> list = new ArrayList<>();
+    private String nextNum;
+    private String midSumCounter;
+    private String midSubCounter;
+    private String midDivCounter;
+    private String midMulCounter;
+    private List<Character> list = new ArrayList<>();
+    private List<String> list2 = new ArrayList<>();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sum=findViewById(R.id.sum);
+        sum = findViewById(R.id.sum);
         sum.setOnClickListener(this);
-        sub=findViewById(R.id.sub);
+        sub = findViewById(R.id.sub);
         sub.setOnClickListener(this);
-        div=findViewById(R.id.div);
+        div = findViewById(R.id.div);
         div.setOnClickListener(this);
-        txt=findViewById(R.id.txt);
-        mul=findViewById(R.id.mul);
+        txt = findViewById(R.id.txt);
+        mul = findViewById(R.id.mul);
         mul.setOnClickListener(this);
-        equal=findViewById(R.id.equal);
+        equal = findViewById(R.id.equal);
         equal.setOnClickListener(this);
-        spot=findViewById(R.id.spot);
+        reset = findViewById(R.id.reset);
+        reset.setOnClickListener(this);
+        spot = findViewById(R.id.spot);
         spot.setOnClickListener(this);
-        num0=findViewById(R.id.num0);
+        num0 = findViewById(R.id.num0);
         num0.setOnClickListener(this);
-        num1=findViewById(R.id.num1);
+        num1 = findViewById(R.id.num1);
         num1.setOnClickListener(this);
-        num2=findViewById(R.id.num2);
+        num2 = findViewById(R.id.num2);
         num2.setOnClickListener(this);
-        num3=findViewById(R.id.num3);
+        num3 = findViewById(R.id.num3);
         num3.setOnClickListener(this);
-        num4=findViewById(R.id.num4);
+        num4 = findViewById(R.id.num4);
         num4.setOnClickListener(this);
-        num5=findViewById(R.id.num5);
+        num5 = findViewById(R.id.num5);
         num5.setOnClickListener(this);
-        num6=findViewById(R.id.num6);
+        num6 = findViewById(R.id.num6);
         num6.setOnClickListener(this);
-        num7=findViewById(R.id.num7);
+        num7 = findViewById(R.id.num7);
         num7.setOnClickListener(this);
-        num8=findViewById(R.id.num8);
+        num8 = findViewById(R.id.num8);
         num8.setOnClickListener(this);
-        num9=findViewById(R.id.num9);
+        num9 = findViewById(R.id.num9);
         num9.setOnClickListener(this);
 
     }
 
-    private String getNums(List<Character> myList){
-        for (char item:myList) {
-            counter+=item;
-        }
-        return counter;
+
+    private String sum() {
+        return Double.toString(Double.parseDouble(midSumCounter) + Double.parseDouble(nextNum));
     }
 
 
+    private String sub() {
+        return Double.toString(Double.parseDouble(midSubCounter) - Double.parseDouble(nextNum));
+    }
 
+
+    private String div() {
+        return Double.toString(Double.parseDouble(midDivCounter) / Double.parseDouble(nextNum));
+    }
+
+
+    private String mul() {
+        return Double.toString(Double.parseDouble(midMulCounter) * Double.parseDouble(nextNum));
+    }
+
+
+    private List<String> getNums() {
+
+        for (Character item : list) {
+            list2.add(item.toString());
+        }
+        return list2;
+    }
+
+    private String listToString() {
+        return TextUtils.join("", getNums());
+    }
+
+
+    private void reset() {
+        getNums().clear();
+    }
+
+    private void equal() {
+        if (!midSumCounter.isEmpty()) {
+            nextNum = listToString();
+            getNums().clear();
+            getNums().add(sum());
+        }
+
+        if (!midSubCounter.isEmpty()) {
+            nextNum = listToString();
+            getNums().clear();
+            getNums().add(sub());
+        }
+
+        if (!midDivCounter.isEmpty()) {
+            nextNum = listToString();
+            getNums().clear();
+            getNums().add(div());
+        }
+
+        if (!midMulCounter.isEmpty()) {
+            nextNum = listToString();
+            getNums().clear();
+            getNums().add(mul());
+        }
+    }
 
 
     @Override
     public void onClick(View view) {
 
-        int id=view.getId();
+        int id = view.getId();
 
-        switch (id){
-            case R.id.num0: list.add('0');
-            break;
-            case R.id.num1: list.add('1');
+        switch (id) {
+            case R.id.num0:
+                list.add('0');
                 break;
-            case R.id.num2: list.add('2');
+            case R.id.num1:
+                list.add('1');
                 break;
-            case R.id.num3: list.add('3');
+            case R.id.num2:
+                list.add('2');
                 break;
-            case R.id.num4: list.add('4');
+            case R.id.num3:
+                list.add('3');
                 break;
-            case R.id.num5: list.add('5');
+            case R.id.num4:
+                list.add('4');
                 break;
-            case R.id.num6: list.add('6');
+            case R.id.num5:
+                list.add('5');
                 break;
-            case R.id.num7: list.add('7');
+            case R.id.num6:
+                list.add('6');
                 break;
-            case R.id.num8: list.add('8');
+            case R.id.num7:
+                list.add('7');
                 break;
-            case R.id.num9: list.add('9');
+            case R.id.num8:
+                list.add('8');
+                break;
+            case R.id.num9:
+                list.add('9');
+                break;
+
+            case R.id.equal:
+                equal();
+                break;
+
+            case R.id.reset:
+                reset();
+                break;
+
+            case R.id.sum:
+                midSubCounter= "";
+                midDivCounter= "";
+                midMulCounter= "";
+                midSumCounter = txt.getText().toString();
+                getNums().clear();
+                break;
+
+            case R.id.sub:
+                midSumCounter = "";
+                midDivCounter= "";
+                midMulCounter= "";
+                midSubCounter = txt.getText().toString();
+                getNums().clear();
+                break;
+
+            case R.id.div:
+                midSumCounter = "";
+                midSubCounter= "";
+                midMulCounter= "";
+                midDivCounter = txt.getText().toString();
+                getNums().clear();
+                break;
+
+            case R.id.mul:
+                midSumCounter = "";
+                midSubCounter= "";
+                midDivCounter= "";
+                midMulCounter = txt.getText().toString();
+                getNums().clear();
                 break;
         }
 
-        txt.setText(getNums(list), TextView.BufferType.EDITABLE);
-
-
-
-    }
-
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-
+        txt.setText(listToString());
+        list.clear();
     }
 
 }
 
-
-//package com.example.user.calculator;
-//
-//import android.annotation.SuppressLint;
-//import android.support.v7.app.AppCompatActivity;
-//import android.os.Bundle;
-//import android.text.TextUtils;
-//import android.view.View;
-//import android.widget.Button;
-//import android.widget.EditText;
-//import android.widget.Toast;
-//
-//public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-//
-//    private EditText number1;
-//    private EditText number2;
-//    private EditText equal;
-//    private Button sum;
-//    private Button sub;
-//    private Button div;
-//    private Button mul;
-//    private String numb1;
-//    private String numb2;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        number1=findViewById(R.id.number1);
-//        number2=findViewById(R.id.number2);
-//        equal=findViewById(R.id.equ);
-//        sum=findViewById(R.id.sum);
-//        sum.setOnClickListener(this);
-//        sub=findViewById(R.id.sub);
-//        sub.setOnClickListener(this);
-//        div=findViewById(R.id.div);
-//        div.setOnClickListener(this);
-//        mul=findViewById(R.id.mul);
-//        mul.setOnClickListener(this);
-//    }
-//
-//    double sum(){
-//        return Double.parseDouble(number1.getText().toString()) + Double.parseDouble(number2.getText().toString());
-//    }
-//
-//    double sub(){
-//        return Double.parseDouble(number1.getText().toString()) - Double.parseDouble(number2.getText().toString());
-//    }
-//
-//    double div(){
-//        return Double.parseDouble(number1.getText().toString()) / Double.parseDouble(number2.getText().toString());
-//    }
-//
-//    double mul(){
-//        return Double.parseDouble(number1.getText().toString()) * Double.parseDouble(number2.getText().toString());
-//    }
-//
-//
-//    @SuppressLint("SetTextI18n")
-//    @Override
-//    public void onClick(View view) {
-//        int id=view.getId();
-//
-//        numb1=number1.getText().toString();
-//        numb2=number2.getText().toString();
-//
-//        if (id==R.id.sum){
-//            if (!TextUtils.isEmpty(numb1) && !TextUtils.isEmpty(numb2)){
-//                if (sum()==(int)sum()){
-//                    equal.setText(Integer.toString((int) sum()));
-//                }else {
-//                    equal.setText(Double.toString(sum()));
-//                }
-//
-//            }else  Toast.makeText(MainActivity.this, "EditText is Empty", Toast.LENGTH_LONG).show();
-//
-//        }
-//
-//        if (id==R.id.sub){
-//            if (!TextUtils.isEmpty(numb1) && !TextUtils.isEmpty(numb2)){
-//                if (sub()==(int)sub()){
-//                    equal.setText(Integer.toString((int) sub()));
-//                }else {
-//                    equal.setText(Double.toString(sub()));
-//                }
-//
-//            }else  Toast.makeText(MainActivity.this, "EditText is Empty", Toast.LENGTH_LONG).show();
-//
-//        }
-//
-//        if (id==R.id.div){
-//            if (!TextUtils.isEmpty(numb1) && !TextUtils.isEmpty(numb2)){
-//                if (div()==(int)div()){
-//                    equal.setText(Integer.toString((int) div()));
-//                }else {
-//                    equal.setText(Double.toString(div()));
-//                }
-//
-//            }else  Toast.makeText(MainActivity.this, "EditText is Empty", Toast.LENGTH_LONG).show();
-//        }
-//
-//        if (id==R.id.mul){
-//            if (!TextUtils.isEmpty(numb1) && !TextUtils.isEmpty(numb2)){
-//                if (mul()==(int)mul()){
-//                    equal.setText(Integer.toString((int) mul()));
-//                }else {
-//                    equal.setText(Double.toString(mul()));
-//                }
-//
-//            }else  Toast.makeText(MainActivity.this, "EditText is Empty", Toast.LENGTH_LONG).show();
-//        }
-//
-//    }
-//
-//    @Override
-//    public void onPointerCaptureChanged(boolean hasCapture) {
-//
-//    }
-//
-//}
